0851b8effce0eed2cb343b7235211db552002941
Cooper <sudomoose@gmail.com>
2019-04-07 21:56:07 -0700

    New parallax stuff

--- src/Routes/Basic.js
+++ src/Routes/Basic.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send(require('../public/html/index.html'))
-});
-
 module.exports = router;
--- src/Server.js
+++ src/Server.js
@@ -3,6 +3,7 @@ const app = express();
 
 const Routes = require('./Routes');
 
+app.use(express.static('public'))
 app.use('/', Routes.Basic);
 
 app.listen(3030);
